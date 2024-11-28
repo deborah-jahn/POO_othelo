2d8bd69a91501f14586c67e054654c21f939cef7
Rabih <rabih.kazma@le-campus-numerique.fr>
2024-11-28 16:38:30 +0100

    la version 2 du tableau , version figée

--- plateau.py
+++ plateau.py
@@ -1,30 +1,51 @@
 
 
 class Plateau:
-    def __init__(self):
+    def __init__(self,taille = int):
 
+        self.taille=taille
         self.grille=[]
-        for row in range(8):
+        for row in range(self.taille):
             d=[]
-            for col in range (8):
+            for col in range (self.taille):
                 d.append(' ')
             self.grille.append(d)
 
-        self.grille[3][3]= 'O'
-        self.grille[4][4]= 'O'
-        self.grille[3][4]= 'X'
-        self.grille[4][3]= 'X'
+        self.grille[int(self.taille/2)-1][int(self.taille/2)-1]= 'O'
+        self.grille[int(self.taille/2)][int(self.taille/2)]= 'O'
+        self.grille[int(self.taille/2)-1][int(self.taille/2)]= 'X'
+        self.grille[int(self.taille/2)][int(self.taille/2)-1]= 'X'
         self.affichage_grille() 
 
      
     def affichage_grille (self):
+
+        
         #print (self.grille)
-        print('    0   1   2   3   4   5   6   7 ')
-        for i in range(8):
-            
-            print('  +---+---+---+---+---+---+---+---+')
-            print(f'{i} | {self.grille[i][0]} | {self.grille[i][1]} | {self.grille[i][2]} | {self.grille[i][3]} | {self.grille[i][4]} | {self.grille[i][5]} | {self.grille[i][6]} | {self.grille[i][7]} |')
-        print('  +---+---+---+---+---+---+---+---+')
+#        print('    0   1   2   3   4   5   6   7 ')
+
+        header = "   "
+        operator = "+---"
+        for i in range(self.taille):
+            header += f" {i}"
+        header = "   " + "  ".join(f" {i}" if len(str(i)) == 1 else f"{i}" for i in range(self.taille))### pour créére une liste de string espacée 
+        ligne  = "  "+"".join([operator] * self.taille)
+        print(header)
+        print (ligne)
+
+        for i in range(self.taille):
+            ligne=""
+            # ligne=str(i)+" "
+            ligne = f" {i}" if len(str(i)) == 1 else f"{i}"
+            texte=0
+            for k in range (self.taille):
+                texte="|  "+str(self.grille[k][i])
+                ligne=ligne+texte
+            ligne=ligne +"|" 
+            print(ligne)
+            # print("   ----------------------------------------------------------------")
+        ligne  = "  "+"".join([operator] * self.taille) + "+"
+        print(ligne)
 
 
 
@@ -34,7 +55,7 @@ class Plateau:
 if __name__=="__main__":
 
 
-    p=Plateau()
+    p=Plateau(4)
 
 
 
